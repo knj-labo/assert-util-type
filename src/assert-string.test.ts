2ecc025d77104adbf6bf7255347e3fc30d78d137
jp-knj <kenji.tomita1996@gmail.com>
2022-08-09 22:57:28 +0900

    :sparkles: use linter to keep

--- src/assert-string.test.ts
+++ src/assert-string.test.ts
@@ -4,10 +4,30 @@ import { serialize } from "./serialize";
 import { assertString } from "./assert-string";
 
 describe('assertFilledString()を使用した場合', () => {
-    describe('true を返すのは', () => {
+    describe('【正常系】true を返すのは', () => {
         ['a'].forEach((v) => {
             test(serialize(v), () => {
                 expect(() => assertString(v, '')).not.toThrow();
+            });
+        });
+    });
+
+    describe('【異常系】false を返すのは', () => {
+        [
+            [],
+            {},
+            1,
+            BigInt(1),
+            null,
+            undefined,
+            true,
+            false,
+            Symbol('symbol'),
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            (): void => {},
+        ].forEach((v) => {
+            test(serialize(v), () => {
+                expect(() => assertString(v, '')).toThrow();
             });
         });
     });
