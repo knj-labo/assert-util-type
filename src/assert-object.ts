38c675f117c345b0428acb7f4b9741e13031b3eb
jp-knj <kenji.tomita1996@gmail.com>
2022-08-10 21:17:10 +0900

    :sparkles: check object type

---
+++ src/assert-object.ts
@@ -0,0 +1,50 @@
+import { PreconditionError } from './precondition-error';
+import { serialize } from "./serialize";
+
+function isObject(value: unknown): value is Record<string, unknown> {
+    if (typeof value !== 'object') {
+        return false;
+    }
+    return value !== null;
+}
+
+function assertObject(
+    value: unknown,
+    target = ''
+): asserts value is Record<string, unknown> {
+    if (!isObject(value)) {
+        throw new PreconditionError(`${target} should be object`.trim());
+    }
+}
+
+if (import.meta.vitest) {
+    const { describe, test, expect } = import.meta.vitest
+    describe('Use case for assertString function', () => {
+        describe('“nomal usecase” the values return true are', () => {
+            [{}, []].forEach((value) => {
+                test(serialize(value), () => {
+                    expect(() => assertObject(value, '')).not.toThrow();
+                });
+            });
+        });
+
+        describe('“exception usecase” the values return false are', () => {
+            [
+                'a',
+                1,
+                BigInt(1),
+                null,
+                undefined,
+                true,
+                false,
+                Symbol('symbol'),
+                // eslint-disable-next-line @typescript-eslint/no-empty-function
+                (): void => {},
+            ].forEach((value) => {
+                test(serialize(value), () => {
+                    expect(() => assertObject(value, '')).toThrow();
+                });
+            });
+        });
+    });
+}
