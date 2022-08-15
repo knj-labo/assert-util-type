54172a166fab994210ea33bc20fb34942f384a88
jp-knj <kenji.tomita1996@gmail.com>
2022-08-16 01:11:41 +0900

    :+1: export assert matched type

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './assert-filled-array';
 export * from './assert-number';
 export * from './assert-object';
 export * from './assert-string';
+export * from './assert-matched-type';
 
 export * from './nominal';
