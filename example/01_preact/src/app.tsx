f3855ec52521e523d31e122eeb434b8c22689fb1
jp-knj <kenji.tomita1996@gmail.com>
2022-08-15 23:02:09 +0900

    :sparkles: how to use assert util type

--- example/01_preact/src/app.tsx
+++ example/01_preact/src/app.tsx
@@ -1,32 +1,25 @@
 import { useState } from 'preact/hooks'
 import preactLogo from './assets/preact.svg'
 import './app.css'
+import { Nominal, assertFilledString} from "assert-util-types";
+
+type FilledString = Nominal<string, 'FilledString'>;
+type TodoId = Nominal<FilledString, 'TodoId'>
+
+function assertTodoId(value: unknown): asserts value is TodoId {
+    assertFilledString(value);
+}
+
+function asToDoId(value: unknown): TodoId {
+    assertTodoId(value);
+    return value;
+}
 
 export function App() {
-  const [count, setCount] = useState(0)
-
+  const id = asToDoId('1');
+  console.log(id)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
     </>
   )
 }
